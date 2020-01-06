20c133c8b5b01a8f048f4e13eb82d0c59891a478
SozonikOleg <Sozonik999@gmail.com>
2020-01-06 17:36:48 +0300

    feat:added new lesson

--- lesson10/index.js
+++ lesson10/index.js
@@ -1,19 +1,23 @@
 const fs = require('fs');
-
-// connect to server
-
 const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   console.log(`Server started on port${req.url}`);
+//   res.writeHead(200, { 'Content-Type': 'text/html; charset= utf-8 ' });
+//   const myReadShort = fs.createReadStream(`${__dirname}/index.html`);
+//   myReadShort.pipe(res);
+// });
 
 const server = http.createServer((req, res) => {
   console.log(`Server started on port${req.url}`);
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset= utf-8 ' });
-
-
-  const myReadShort = fs.createReadStream(`${__dirname}/article10.txt`);
-
-  myReadShort.pipe(res);
+  res.writeHead(200, { 'Content-Type': 'application/json; charset= utf-8 ' });
+  const obj = {
+    model: 'Audi',
+    speed: '234.4',
+    Wheels: 4,
+  };
+  res.end(JSON.stringify(obj));
 });
 
 server.listen(3000, '127.0.0.1');
-
 console.log('Listen port 3000!');
---
+++ lesson11/index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  console.log(`Server started on port${req.url}`);
+
+  if (req.url === '/index' || req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset= utf-8 ' });
+    fs.createReadStream(`${__dirname}/index.html`).pipe(res);
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset= utf-8 ' });
+    fs.createReadStream(`${__dirname}/about.html`).pipe(res);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html; charset= utf-8 ' });
+    fs.createReadStream(`${__dirname}/404.html`).pipe(res);
+  }
+});
+
+server.listen(3000, '127.0.0.1');
+
+console.log('Listen port 3000!');
